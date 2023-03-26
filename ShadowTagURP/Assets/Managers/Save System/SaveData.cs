8056a913bd3d3e68aac03821253282c2acb6cc34
Cur1o <2021gmehersan@gmail.com>
2023-03-26 23:16:34 +0200

    update portal and house

--- ShadowTagURP/Assets/Managers/Save System/SaveData.cs
+++ ShadowTagURP/Assets/Managers/Save System/SaveData.cs
@@ -10,7 +10,8 @@ public class SaveData
     public int[] points;                //The Points from all levels
     public int currentLabyrinthLevel;   //The current Labyrinth level
     public Vector3 playerPosition;      //The current player Position
-    public List<bool> unlockedWeapons;       //To save all the weapons that are Unlocked
+    public List<bool> unlockedWeapons;  //To save all the weapons that are Unlocked
+    public List<bool> unlockedLevels;   //Unlocked Levels   
     public SaveData()   //The variables are set to this numbers at a new game
     {
         this.currentPoints = 0;
@@ -18,5 +19,6 @@ public class SaveData
         this.currentLabyrinthLevel = 0;
         this.playerPosition = Vector3.zero;  
         this.unlockedWeapons = new List<bool> {false,false,false,false};
+        this.unlockedLevels = new List<bool> { true, false, false, false };
     }
 }
--- ShadowTagURP/Assets/Prefabs/Buildings/Portal/Portal.cs
+++ ShadowTagURP/Assets/Prefabs/Buildings/Portal/Portal.cs
@@ -2,14 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class Portal : MonoBehaviour , IDataPersistance
 {
+    [SerializeField] private bool isHub;
+    [SerializeField] public bool isUnlocked;
+    [SerializeField] ScenesManager.Scene level;
+    [SerializeField] ScenesManager.Scene currentLevel;
+    private void Start()
+    {
+        SaveManager.Instance.dataPersistenceObjects.Add(this);
+        LoadData(SaveManager.Instance.gameData);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        SaveManager.Instance.gameData.points[SaveManager.Instance.gameData.currentLabyrinthLevel] = SaveManager.Instance.gameData.currentPoints;
-        SaveManager.Instance.gameData.currentLabyrinthLevel++;
-        SaveManager.Instance.gameData.playerPosition = Vector3.zero;
-        PlayerUI.Instance.UpdateLevel();
-        ScenesManager.Instance.LoadNextScene();
+        if (!isHub)
+        {
+            SaveManager.Instance.gameData.points[SaveManager.Instance.gameData.currentLabyrinthLevel] = SaveManager.Instance.gameData.currentPoints;
+            isUnlocked = true;
+            SaveManager.Instance.gameData.currentLabyrinthLevel++;
+            SaveManager.Instance.gameData.playerPosition = Vector3.zero;
+            PlayerUI.Instance.UpdateLevel();
+            ScenesManager.Instance.LoadNextScene();
+        }
+        else if(isHub && isUnlocked)
+        {
+            SaveManager.Instance.gameData.currentLabyrinthLevel = (int)level;
+            SaveManager.Instance.gameData.playerPosition = Vector3.zero;
+            PlayerUI.Instance.UpdateLevel();
+            ScenesManager.Instance.LoadScene(level);
+        }else if (!isUnlocked)
+        {
+            gameObject.SetActive(false);
+        }
     }
+    public void SaveData(ref SaveData data) => data.unlockedLevels[(int)currentLevel] = this.isUnlocked;
+    public void LoadData(SaveData data) => this.isUnlocked = data.unlockedLevels[(int)currentLevel];
 }
